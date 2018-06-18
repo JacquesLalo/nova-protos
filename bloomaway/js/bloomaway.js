121df753288ff42bfaa30f892e396a410cd96ea0
Thomas Binetruy <tbinetruy@gmail.com>
2018-06-19 00:13:06 +0200

    refactored

--- bloomaway/js/bloomaway.js
+++ bloomaway/js/bloomaway.js
@@ -13,43 +13,76 @@ class Bloomaway {
         this.init = this.init.bind(this)
         this.animate = this.animate.bind(this)
         this.onWindowResize = this.onWindowResize.bind(this)
+        this.getObj = this.getObj.bind(this)
+        this.getGltf = this.getGltf.bind(this)
+        this.initCamera = this.initCamera.bind(this)
+        this.initLight = this.initLight.bind(this)
+        this.initScene = this.initScene.bind(this)
+        this.initRenderer = this.initRenderer.bind(this)
 
         this.init()
         this.animate()
     }
 
     init() {
+        // Bind to DOM
 				this.container = document.createElement( 'div' );
 				document.body.appendChild( this.container );
-				this.camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 0.25, 20 );
+				window.addEventListener( 'resize', this.onWindowResize, false );
+
+        this.initScene()
+        this.initCamera()
+        this.initLight()
+        this.initRenderer()
+
+				// Models
+        this.getObj('torus')
+        this.getGltf('bateau/scene')
+
+    }
+    initRenderer() {
+				this.renderer = new THREE.WebGLRenderer( { antialias: true } );
+				this.renderer.setPixelRatio( window.devicePixelRatio );
+				this.renderer.setSize( window.innerWidth, window.innerHeight );
+				this.renderer.gammaOutput = true;
+				this.container.appendChild( this.renderer.domElement );
+    }
+    initScene() {
 				this.scene = new THREE.Scene();
+    }
+    initLight() {
 				this.light = new THREE.HemisphereLight( 0xbbbbff, 0x444422 );
 				this.light.position.set( 0, 1, 0 );
 				this.scene.add( this.light );
-
-
+    }
+    initCamera() {
+				this.camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 0.25, 20 );
+				this.camera.position.set( -0.2, -13, 0 );
+        this.scene.add( this.camera );
+    }
+    getGltf(name) {
         var loader = new THREE.GLTFLoader();
-        loader.load( 'gltf/bateau/scene.gltf', gltf => {
-             this.gltf = gltf
-             const s = 0.001
-             this.gltf.scene.scale.set(s, s, s);
-             this.gltf.scene.position.x = 0;				    //Position (x = right+ left-)
-             this.gltf.scene.position.y = 0;				    //Position (y = up+, down-)
-             this.gltf.scene.position.z = 0;
-         	   this.scene.add( this.gltf.scene );
+        loader.load( 'gltf/' + name + '.gltf', gltf => {
+            this.gltf = gltf
+            const s = 0.001
+            this.gltf.scene.scale.set(s, s, s);
+            this.gltf.scene.position.x = 0;				    //Position (x = right+ left-)
+            this.gltf.scene.position.y = 0;				    //Position (y = up+, down-)
+            this.gltf.scene.position.z = 0;
+         	  this.scene.add( this.gltf.scene );
         } );
-
-				// model
+    }
+    getObj(name) {
         const onProgress = () => {}
         const onError = () => {}
 				new THREE.MTLLoader()
             .setPath( 'obj/' )
-            .load( 'torus.mtl', function ( materials ) {
+            .load( name + '.mtl', function ( materials ) {
                 materials.preload();
                 new THREE.OBJLoader()
                     .setMaterials( materials )
                     .setPath( 'obj/' )
-                    .load( 'torus.obj', function ( object ) {
+                    .load( name + '.obj', function ( object ) {
                         object.position.x = 0;
                         object.position.y = 0;
                         object.position.z = 0;
@@ -63,18 +96,6 @@ class Bloomaway {
                         this.obj = object
                     }, onProgress, onError );
             } );
-
-				this.renderer = new THREE.WebGLRenderer( { antialias: true } );
-				this.renderer.setPixelRatio( window.devicePixelRatio );
-				this.renderer.setSize( window.innerWidth, window.innerHeight );
-				this.renderer.gammaOutput = true;
-				this.container.appendChild( this.renderer.domElement );
-				window.addEventListener( 'resize', this.onWindowResize, false );
-
-        var geometry = new THREE.BoxGeometry( 1, 1, 1 );
-        var material = new THREE.MeshBasicMaterial( {color: 0x00ff00} );
-				this.camera.position.set( -0.2, -13, 0 );
-        this.scene.add( this.camera );
     }
 		onWindowResize() {
 				this.camera.aspect = window.innerWidth / window.innerHeight;

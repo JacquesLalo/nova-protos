c7bce80e50394c2d891b3c62be23d9080445bf03
Thomas Binetruy <tbinetruy@gmail.com>
2018-06-19 23:42:25 +0200

    quick fix

--- bloomaway/js/camera.js
+++ bloomaway/js/camera.js
@@ -34,8 +34,8 @@ class Camera {
         this.init()
     }
     init() {
-				this.camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 40)
-				this.camera.position.set(0, 0, 0)
+        this.camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 40)
+        this.camera.position.set(0, 0, 0)
         this.camera.lookAt(new THREE.Vector3(-1, 0, 0))
         this.scene.add(this.camera)
 

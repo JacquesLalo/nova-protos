7e6156be7adf427643716267939d2c370a906bbc
Thomas Binetruy <tbinetruy@gmail.com>
2018-06-28 17:29:51 +0200

    refactired audioXR js into class

--- audioXR/js/audioXR.js
+++ audioXR/js/audioXR.js
@@ -1,101 +1,109 @@
-var renderer, scene, camera
-var uniforms
-var displacement
-var geometry, plane
+class audioXR {
+    constructor() {
+        this.renderer = null
+        this.scene = null
+        this.camera = null
+        this.uniforms = null
+        this.displacement = null
+        this.geometry = null
+        this.plane = null
 
-init()
-animate()
+        this.init = this.init.bind(this)
+        this.animate = this.animate.bind(this)
+        this.applyDisplacements = this.applyDisplacements.bind(this)
+        this.onWindowResize = this.onWindowResize.bind(this)
+        this.render = this.render.bind(this)
 
-// Displaces calculates displacements for each vertices of a mesh
-function applyDisplacements(displacement) {
-    for (var i = 0; i < displacement.length; i++) {
-        if(window.data && i % 2 === 0) {
-            const x = window.data[i % window.data.length] / 128
-            displacement[i] = 0.2 * Math.pow(x, 2)
+        this.init()
+        this.animate()
+    }
+    init() {
+        // Camera setup
+        this.camera = new THREE.PerspectiveCamera(30, window.innerWidth / window.innerHeight, 1, 10000)
+        this.camera.position.z = 60
+        this.camera.position.x = 5
+
+        // Scene setup
+        this.scene = new THREE.Scene()
+
+        // Shader variables setup
+        this.uniforms = {
+            amplitude: {
+                value: 10,
+            },
+            scaleFactor: {
+                value: 1.0,
+            },
+            texture: {
+                value: new THREE.TextureLoader().load('textures/about-us.png'),
+            }
         }
+        this.uniforms.texture.value.wrapS = this.uniforms.texture.value.wrapT = THREE.MirroredRepeatWrapping
+        const shaderMaterial = new THREE.ShaderMaterial({
+            uniforms: this.uniforms,
+            vertexShader: document.getElementById('vertexshader').textContent,
+            fragmentShader: document.getElementById('fragmentshader').textContent,
+            side: THREE.DoubleSide,
+        })
+
+        // setting up the geometry
+        // play with controls on: https://threejs.org/docs/#api/geometries/PlaneBufferGeometry
+        this.geometry = new THREE.SphereBufferGeometry(5, 32, 32)
+
+        // Deforming plane with displacements
+        this.displacement = new Float32Array(this.geometry.attributes.position.count)
+        this.applyDisplacements(this.displacement)
+
+        // Setting up the shaders' attributes
+        this.geometry.addAttribute('displacement', new THREE.BufferAttribute(this.displacement, 1))
+
+        // Creating the mesh
+        this.plane = new THREE.Mesh(this.geometry, shaderMaterial)
+
+        // Adding the mesh to our scene
+        this.scene.add(this.plane)
+
+        // Rendering
+        this.renderer = new THREE.WebGLRenderer()
+        this.renderer.setPixelRatio(window.devicePixelRatio)
+        this.renderer.setSize(window.innerWidth, window.innerHeight)
+        const container = document.getElementById('container')
+        container.appendChild(this.renderer.domElement)
+
+        // Event listeners
+        window.addEventListener('resize', this.onWindowResize, false)
+    }
+    onWindowResize() {
+        this.camera.aspect = window.innerWidth / window.innerHeight
+        this.camera.updateProjectionMatrix()
+        this.renderer.setSize(window.innerWidth, window.innerHeight)
+    }
+    applyDisplacements(displacement) {
+        for (let i = 0; i < displacement.length; i++) {
+            if(window.data && i % 2 === 0) {
+                const x = window.data[i % window.data.length] / 128
+                displacement[i] = 0.2 * Math.pow(x, 2)
+            }
+        }
+    }
+    animate() {
+        requestAnimationFrame(this.animate)
+        this.render()
+    }
+    render() {
+        const time = Date.now() * 0.001
+
+        // Rotating and scaling plane wrt time
+        this.plane.rotation.z = 0.5 * time
+        this.plane.rotation.y = 0.5 * time
+        this.uniforms.scaleFactor.value = 1
+
+        this.applyDisplacements(this.displacement)
+        this.geometry.attributes.displacement.needsUpdate = true
+
+        // Render
+        this.renderer.render(this.scene, this.camera)
     }
 }
 
-function init() {
-    // Camera setup
-    camera = new THREE.PerspectiveCamera(30, window.innerWidth / window.innerHeight, 1, 10000)
-    camera.position.z = 60
-    camera.position.x = 5
-
-    // Scene setup
-    scene = new THREE.Scene()
-
-    // Shader variables setup
-    uniforms = {
-        amplitude: {
-            value: 10,
-        },
-        scaleFactor: {
-            value: 1.0,
-        },
-        texture: {
-            value: new THREE.TextureLoader().load('textures/about-us.png'),
-        }
-    }
-    uniforms.texture.value.wrapS = uniforms.texture.value.wrapT = THREE.MirroredRepeatWrapping
-    var shaderMaterial = new THREE.ShaderMaterial({
-        uniforms: uniforms,
-        vertexShader: document.getElementById('vertexshader').textContent,
-        fragmentShader: document.getElementById('fragmentshader').textContent,
-        side: THREE.DoubleSide,
-    })
-
-    // setting up the geometry
-    // play with controls on: https://threejs.org/docs/#api/geometries/PlaneBufferGeometry
-    geometry = new THREE.SphereBufferGeometry(5, 32, 32)
-
-    // Deforming plane with displacements
-    displacement = new Float32Array(geometry.attributes.position.count)
-    applyDisplacements(displacement)
-
-    // Setting up the shaders' attributes
-    geometry.addAttribute('displacement', new THREE.BufferAttribute(displacement, 1))
-
-    // Creating the mesh
-    plane = new THREE.Mesh(geometry, shaderMaterial)
-
-    // Adding the mesh to our scene
-    scene.add(plane)
-
-    // Rendering
-    renderer = new THREE.WebGLRenderer()
-    renderer.setPixelRatio(window.devicePixelRatio)
-    renderer.setSize(window.innerWidth, window.innerHeight)
-    var container = document.getElementById('container')
-    container.appendChild(renderer.domElement)
-
-    // Event listeners
-    window.addEventListener('resize', onWindowResize, false)
-}
-
-function onWindowResize() {
-    camera.aspect = window.innerWidth / window.innerHeight
-    camera.updateProjectionMatrix()
-    renderer.setSize(window.innerWidth, window.innerHeight)
-}
-
-function animate() {
-    requestAnimationFrame(animate)
-    render()
-}
-
-function render() {
-    var time = Date.now() * 0.001
-
-    // Rotating and scaling plane wrt time
-    plane.rotation.z = 0.5 * time
-    plane.rotation.y = 0.5 * time
-    uniforms.scaleFactor.value = 1 //0.5 * Math.sin(time)
-
-
-    applyDisplacements(displacement)
-    geometry.attributes.displacement.needsUpdate = true
-
-    // Render
-    renderer.render(scene, camera)
-}
+window.audioXR = new audioXR()

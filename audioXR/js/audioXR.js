efe497c688709bb932f9cabc7923da4228e1e14a
Thomas Binetruy <tbinetruy@gmail.com>
2018-06-29 19:54:28 +0200

    refactored audioXR/index.html into audioXR.html at root of repo
    so we can use torus

--- audioXR/js/audioXR.js
+++ audioXR/js/audioXR.js
@@ -1,5 +1,5 @@
-import Torus from '../../bloomaway/js/torus.js'
 import Controls from '../../bloomaway/js/controls.js'
+import Torus from '../../bloomaway/js/torus.js'
 
 class audioXR {
     constructor() {
@@ -14,7 +14,6 @@ class audioXR {
         this.controls = null
 
         this.init = this.init.bind(this)
-        this.init = this.init.bind(this)
         this.animate = this.animate.bind(this)
         this.applyDisplacements = this.applyDisplacements.bind(this)
         this.onWindowResize = this.onWindowResize.bind(this)
@@ -26,13 +25,27 @@ class audioXR {
     init() {
         // Camera setup
         this.camera = new THREE.PerspectiveCamera(30, window.innerWidth / window.innerHeight, 1, 10000)
-        this.camera.position.z = 60
-        this.camera.position.x = 5
+        this.camera.position.set(20, 0, 60)
+
 
         // Scene setup
         this.scene = new THREE.Scene()
 
+        // setup light
+        this.light = new THREE.HemisphereLight(0xbbbbff, 0x444422)
+        this.light.position.set(0, 1, 0)
+        this.scene.add(this.light)
+
+        // Setup Torus
         this.controls = new Controls(this.camera, this.scene)
+        this.torus = new Torus(this.scene, this.controls)
+
+
+        this.torus.createButton(() => {}, {
+            position: new THREE.Vector3(-1.5, 0, 0),
+            scale: 0.5,
+            shape: 'box',
+        })
 
         // Shader variables setup
         this.uniforms = {
@@ -43,7 +56,7 @@ class audioXR {
                 value: 1.0,
             },
             texture: {
-                value: new THREE.TextureLoader().load('textures/about-us.png'),
+                value: new THREE.TextureLoader().load('audioXR/textures/about-us.png'),
             }
         }
         this.uniforms.texture.value.wrapS = this.uniforms.texture.value.wrapT = THREE.MirroredRepeatWrapping
@@ -56,7 +69,7 @@ class audioXR {
 
         // setting up the geometry
         // play with controls on: https://threejs.org/docs/#api/geometries/PlaneBufferGeometry
-        this.geometry = new THREE.SphereBufferGeometry(5, 32, 32)
+        this.geometry = new THREE.SphereBufferGeometry(0.1, 32, 32)
 
         // Deforming plane with displacements
         this.displacement = new Float32Array(this.geometry.attributes.position.count)
@@ -67,6 +80,8 @@ class audioXR {
 
         // Creating the mesh
         this.plane = new THREE.Mesh(this.geometry, shaderMaterial)
+        this.plane.position.y = 3
+        this.plane.position.x = 3
 
         // Adding the mesh to our scene
         this.scene.add(this.plane)

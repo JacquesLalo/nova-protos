6d6062f468c78b586665f045740b3ebf6f64fd5b
Thomas Binetruy <tbinetruy@gmail.com>
2018-07-11 23:38:28 +0200

    added template boilerplate

---
+++ template/js/template.js
@@ -0,0 +1,23 @@
+import Super from '../../engine/super.js'
+
+class audioXR extends Super {
+    constructor() {
+        //calling Super constructor
+        super()
+
+        //initializing attributes
+
+        //bindings
+
+        this.init()
+        super.animate()
+    }
+    init() {
+        super.init()
+    }
+    animate() {
+        super.animate()
+    }
+}
+
+window.audioXR = new audioXR()

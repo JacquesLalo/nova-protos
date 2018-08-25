9337d63b28ee3b3580c3a048aa117bc6bcc33b6b
Thomas Binetruy <tbinetruy@gmail.com>
2018-08-25 17:56:23 -0400

    updated kartell

--- kartell/js/kartell.tsx
+++ kartell/js/kartell.tsx
@@ -27,25 +27,10 @@ class App extends React.Component<{}, AppState> {
   }
   render() {
     return (
-      <a-scene>
-        <a-box position="-1 0.5 -3" rotation="0 45 0" color="#4CC3D9" />
-        <a-sphere position="0 1.25 -5" radius="1.25" color="#EF2D5E" />
-        <a-cylinder
-          position="1 0.75 -3"
-          radius={this.state.scale}
-          height="1.5"
-          color="#FFC65D"
-        />
-        <a-plane
-          position="0 0 -4"
-          rotation="-90 0 0"
-          width="4"
-          height="4"
-          color="#7BC8A4"
-        />
+      <a-scene vr-mode-ui="enabled: true">
         <a-obj-model
-          src="./kartell/obj/office.obj"
-          mtl="./kartell/obj/office.mtl"
+          src="./kartell/obj/kartell-room.obj"
+          mtl="./kartell/obj/kartell-room.mtl"
         />
         <a-sky color="#ECECEC" />
         <a-entity laser-controls="hand: left" />

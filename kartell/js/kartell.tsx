44aa1b939a194a61c7408a3a8120979e8053d88c
myarmolinsky <myarmolinsky3043@bths.edu>
2018-08-29 02:02:36 -0400

    moving chair around possible

--- kartell/js/kartell.tsx
+++ kartell/js/kartell.tsx
@@ -3,7 +3,12 @@ import * as ReactDOM from "react-dom";
 import * as React from "react";
 import "aframe";
 
-const Model = (props: {isSelected: boolean, position: string}) => (
+const Model = (props: {
+    isSelected: boolean,
+    position: string,
+    collisionBoxScale: string,
+    collisionBoxPosition: string,
+}) => (
   <a-entity position={props.position}>
     <a-obj-model
       id="model"
@@ -16,10 +21,10 @@ const Model = (props: {isSelected: boolean, position: string}) => (
       class="collidable"
       geometry="primitive: box"
       material={
-        "color: rgba(0, 0, 0); opacity: " + (props.isSelected ? "0.3" : "0.1")
+        "color: rgba(0, 0, 0); opacity: " + (props.isSelected ? "0.3" : "0.0")
       }
-      scale="1.5 1.5 0.8"
-      position="0 0.8 0"
+      scale={ props.collisionBoxScale }
+      position={ props.collisionBoxPosition }
     />
   </a-entity>
 );
@@ -38,14 +43,12 @@ const defaultState = {
 
 class App extends React.Component<{}, AppState> {
   raq: number;
-  raycastingPosition: THREE.Vector3;
   modelDistance: number;
   constructor(props) {
     super(props);
 
     this.state = defaultState;
 
-    this.raycastingPosition = new THREE.Vector3(0, 0, 0)
     this.modelDistance = 0
 
     this.update = this.update.bind(this);
@@ -54,7 +57,6 @@ class App extends React.Component<{}, AppState> {
     this.update();
 
     document.addEventListener("triggerdown", () => {
-      console.log(this.state.modelPosition)
       this.setState({triggerDown: true});
     });
 
@@ -66,8 +68,6 @@ class App extends React.Component<{}, AppState> {
       const {el, intersection} = e.detail;
 
       this.modelDistance = intersection.distance;
-        console.log(this.modelDistance)
-      this.raycastingPosition = el.object3D.position;
 
       this.setState({intersection: true});
     });
@@ -80,11 +80,11 @@ class App extends React.Component<{}, AppState> {
     );
 
     document.addEventListener("trackpaddown", (e: CustomEvent) => {
-      if (this.isSelected()) this.modelDistance += 0.1;
+      if (this.isSelected()) this.modelDistance += 0.25;
         console.log(this.modelDistance)
     });
     document.addEventListener("thumbstickdown", (e: CustomEvent) => {
-      if (this.isSelected()) this.modelDistance -= 0.1;
+      if (this.isSelected()) this.modelDistance -= 0.25;
     });
   }
 
@@ -120,6 +120,8 @@ class App extends React.Component<{}, AppState> {
           mtl="./kartell/obj/kartell-room.mtl"
         />
         <Model
+          collisionBoxPosition="0 0.55 0"
+          collisionBoxScale="0.85 1.171 0.8"
           position={modelPosition}
           isSelected={this.isSelected()} />
         <a-obj-model

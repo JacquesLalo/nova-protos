c55430c2bd2c74dae80cc6b305dd026f8125d4d5
Thomas Binetruy <tbinetruy@gmail.com>
2018-08-30 19:42:57 -0400

    wip bloomaway afram

--- bloomaway/js/bloomaway_aframe.tsx
+++ bloomaway/js/bloomaway_aframe.tsx
@@ -2,9 +2,17 @@ import * as ReactDOM from "react-dom";
 import * as React from "react";
 import "aframe";
 
-export interface AppState {}
+export interface AppState {
+  triggerDown: boolean;
+  hoveredButton: string;
+  currentScene: number;
+}
 
-const defaultState = {};
+const defaultState = {
+  triggerDown: false,
+  hoveredButton: "",
+  currentScene: 0,
+};
 
 const {PI, sin, cos} = Math;
 
@@ -17,6 +25,8 @@ const sphericalToCartesian = (theta: number, phi: number, rho: number) => ({
 class App extends React.Component<{}, AppState> {
   raq: number;
   buttonCoords: Array<{theta: number; phi: number}>;
+  scenes: Array<string>;
+
   constructor(props) {
     super(props);
 
@@ -28,17 +38,41 @@ class App extends React.Component<{}, AppState> {
       {theta: -PI / 10, phi: PI / 3},
       {theta: -PI / 10, phi: -PI / 3},
     ];
+    this.scenes = ["stadium"];
 
     this.update = this.update.bind(this);
+    this.onTriggerUp = this.onTriggerUp.bind(this);
+    this.onTriggerDown = this.onTriggerDown.bind(this);
     this.getButtons = this.getButtons.bind(this);
 
     this.update();
 
-    document.addEventListener("thumbstickmoved", (e: CustomEvent) => {});
-    document.addEventListener("triggerdown", () => {});
-    document.addEventListener("triggerup", () => {});
-    document.addEventListener("trackpaddown", (e: CustomEvent) => {});
-    document.addEventListener("thumbstickdown", (e: CustomEvent) => {});
+    document.addEventListener("triggerdown", this.onTriggerDown);
+    document.addEventListener("triggerup", this.onTriggerUp);
+    document.addEventListener("raycaster-intersected", this.onTriggerUp);
+    document.addEventListener(
+      "raycaster-intersected-cleared",
+      this.onTriggerUp,
+    );
+  }
+  onRaycasterIntersected(e: CustomEvent) {
+    this.setState({hoveredButton: e.detail.el.id});
+  }
+  onRaycasterIntersectedCleared(e: CustomEvent) {
+    this.setState({hoveredButton: defaultState.hoveredButton});
+  }
+  onTriggerDown() {
+    if (this.state.hoveredButton !== defaultState.hoveredButton) {
+      const b = this.state.hoveredButton;
+      this.setState({currentScene: this.scenes[b[b.length - 1]]});
+    }
+  }
+
+  onTriggerUp() {
+    this.setState({
+      triggerDown: false,
+      hoveredButton: defaultState.hoveredButton,
+    });
   }
 
   update() {
@@ -49,31 +83,39 @@ class App extends React.Component<{}, AppState> {
   getButtons() {
     return this.buttonCoords.map(({phi, theta}, i) => {
       const cart = sphericalToCartesian(theta, phi, 0.09);
+      const id = `button${i}`;
 
       return (
         <a-sphere
           color="yellow"
           radius="0.005"
           position={`${cart.x} ${cart.y} ${cart.z}`}
-          opacity=" 0.3"
+          opacity={id === this.state.hoveredButton ? "1" : 0.3}
           key={i}
-          class={`torus-button`}
+          className={`torus-button`}
+          id={id}
         />
       );
     });
   }
   render() {
+    const currentScene = this.scenes[this.state.currentScene];
+    console.log("hello", currentScene);
     return (
       <a-scene>
+        <a-entity
+          gltf-model={`url(./bloomaway/gltf/${currentScene}/scene.gltf)`}
+          position="0 1.63 -3.3"
+          scale="0.3 0.3 0.3"
+        />
+
         <a-entity rotation="0 -90 0" scale="3 3 3" position="0 1.6 0">
           <a-obj-model src="./torus/obj/map/map.obj" />
           <a-obj-model src="./torus/obj/ground/ground.obj" />
-          <a-obj-model src="./torus/obj/shell/shell.obj" />
           <a-entity rotation="0 -90 0" position="0 0 0">
             {this.getButtons()}
           </a-entity>
         </a-entity>
-        <a-sky color="#ECECEC" />
         <a-entity
           laser-controls="hand: right"
           raycaster="objects: .torus-button; recursive: true"

1519367c24dbd031af93cb145a60f5c50dea8410
myarmolinsky <myarmolinsky3043@bths.edu>
2018-08-31 14:50:14 -0400

    finished bloomaway prototype

--- bloomaway/js/bloomaway_aframe.tsx
+++ bloomaway/js/bloomaway_aframe.tsx
@@ -16,6 +16,12 @@ const defaultState = {
 
 const {PI, sin, cos} = Math;
 
+const createScene = (name: string, position: string = "0 0 0", scale: string = "1 1 1") => ({
+    name,
+    position,
+    scale,
+})
+
 const sphericalToCartesian = (theta: number, phi: number, rho: number) => ({
   x: rho * sin(phi) * cos(theta),
   y: rho * sin(phi) * sin(theta),
@@ -25,22 +31,28 @@ const sphericalToCartesian = (theta: number, phi: number, rho: number) => ({
 class App extends React.Component<{}, AppState> {
   raq: number;
   buttonCoords: Array<{theta: number; phi: number}>;
-  scenes: Array<string>;
+  scenes: Array<{name: string, position: string, scale: string}>;
 
   constructor(props) {
     super(props);
 
     this.state = defaultState;
     this.buttonCoords = [
-      {theta: 0, phi: 0},
       {theta: PI / 5, phi: PI / 3},
-      {theta: PI / 10, phi: -PI / 3},
-      {theta: -PI / 10, phi: PI / 3},
-      {theta: -PI / 10, phi: -PI / 3},
+        {theta: 0, phi: 0},
+      // {theta: PI / 10, phi: -PI / 3},
+      // {theta: -PI / 10, phi: PI / 3},
+      // {theta: -PI / 10, phi: -PI / 3},
     ];
-    this.scenes = ["stadium"];
+      this.scenes = [
+          createScene("stadium", "0 -3.639 0", "0.02 0.02 0.02"),
+          createScene("king", "0 1.63 -8.3", "1 1 1"),
+          createScene("bedroom1"),
+      ];
 
     this.update = this.update.bind(this);
+    this.onRaycasterIntersected = this.onRaycasterIntersected.bind(this);
+    this.onRaycasterIntersectedCleared = this.onRaycasterIntersectedCleared.bind(this);
     this.onTriggerUp = this.onTriggerUp.bind(this);
     this.onTriggerDown = this.onTriggerDown.bind(this);
     this.getButtons = this.getButtons.bind(this);
@@ -49,14 +61,14 @@ class App extends React.Component<{}, AppState> {
 
     document.addEventListener("triggerdown", this.onTriggerDown);
     document.addEventListener("triggerup", this.onTriggerUp);
-    document.addEventListener("raycaster-intersected", this.onTriggerUp);
+    document.addEventListener("raycaster-intersected", this.onRaycasterIntersected);
     document.addEventListener(
       "raycaster-intersected-cleared",
-      this.onTriggerUp,
+      this.onRaycasterIntersectedCleared,
     );
   }
   onRaycasterIntersected(e: CustomEvent) {
-    this.setState({hoveredButton: e.detail.el.id});
+    this.setState({hoveredButton: e.detail.intersection.object.el.id});
   }
   onRaycasterIntersectedCleared(e: CustomEvent) {
     this.setState({hoveredButton: defaultState.hoveredButton});
@@ -64,7 +76,9 @@ class App extends React.Component<{}, AppState> {
   onTriggerDown() {
     if (this.state.hoveredButton !== defaultState.hoveredButton) {
       const b = this.state.hoveredButton;
-      this.setState({currentScene: this.scenes[b[b.length - 1]]});
+        const currentScene = parseInt(b[b.length - 1])
+        console.log("currentscene", currentScene)
+      this.setState({currentScene});
     }
   }
 
@@ -82,35 +96,37 @@ class App extends React.Component<{}, AppState> {
   }
   getButtons() {
     return this.buttonCoords.map(({phi, theta}, i) => {
-      const cart = sphericalToCartesian(theta, phi, 0.09);
+      const cart = sphericalToCartesian(theta, phi, 0.07);
       const id = `button${i}`;
 
       return (
         <a-sphere
-          color="yellow"
+          color="black"
           radius="0.005"
           position={`${cart.x} ${cart.y} ${cart.z}`}
           opacity={id === this.state.hoveredButton ? "1" : 0.3}
           key={i}
-          className={`torus-button`}
+          class="torus-button"
           id={id}
+          material="side: double"
         />
       );
     });
   }
   render() {
-    const currentScene = this.scenes[this.state.currentScene];
-    console.log("hello", currentScene);
+    const scene = this.scenes[this.state.currentScene];
+      console.log(scene.name)
     return (
       <a-scene>
         <a-entity
-          gltf-model={`url(./bloomaway/gltf/${currentScene}/scene.gltf)`}
-          position="0 1.63 -3.3"
-          scale="0.3 0.3 0.3"
+          gltf-model={`url(./bloomaway/gltf/${scene.name}/scene.gltf)`}
+        position={ scene.position }
+        scale={ scene.scale}
         />
-
-        <a-entity rotation="0 -90 0" scale="3 3 3" position="0 1.6 0">
-          <a-obj-model src="./torus/obj/map/map.obj" />
+        <a-entity rotation="0 -90 0" scale="10 10 10" position="0 1.6 0">
+            <a-entity
+                gltf-model={`url(./torus/gltf/map.gltf)`}
+            />
           <a-obj-model src="./torus/obj/ground/ground.obj" />
           <a-entity rotation="0 -90 0" position="0 0 0">
             {this.getButtons()}

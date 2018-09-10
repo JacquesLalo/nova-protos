98c2944732add5a115c3933f55c88657ade323b0
Thomas Binetruy <tbinetruy@gmail.com>
2018-09-09 23:49:49 -0400

    linting

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,38 +1,26 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "commonjs": true,
-        "es6": true
+  env: {
+    browser: true,
+    commonjs: true,
+    es6: true,
+  },
+  extends: "eslint:recommended",
+  parserOptions: {
+    ecmaVersion: 2018,
+    ecmaFeatures: {
+      jsx: true,
     },
-    "extends": "eslint:recommended",
-    "parserOptions": {
-        "ecmaVersion": 2018,
-        "ecmaFeatures": {
-            "jsx": true
-        },
-        "sourceType": "module"
-    },
-    "plugins": [
-        "react"
-    ],
-    "globals": {
-        "THREE": true,
-    },
-    "rules": {
-        "indent": [
-            "error",
-            4,
-            { SwitchCase: 1 }
-        ],
+    sourceType: "module",
+  },
+  plugins: ["react"],
+  globals: {
+    THREE: true,
+  },
+  rules: {
+    indent: ["error", 4, {SwitchCase: 1}],
 
-        "quotes": [
-            "error",
-            "single"
-        ],
-        "semi": [
-            "error",
-            "never"
-        ],
-        "no-trailing-spaces": ["error", { "ignoreComments": false }],
-    }
+    quotes: ["error", "single"],
+    semi: ["error", "never"],
+    "no-trailing-spaces": ["error", {ignoreComments: false}],
+  },
 };
